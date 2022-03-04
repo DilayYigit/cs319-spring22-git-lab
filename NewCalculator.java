4f9528aec59692cf32a5979f524ead078df69c71
Dilay Yigit <dilayigt@gmail.com>
2022-03-04 12:08:21 +0300

    fix division operator

--- NewCalculator.java
+++ NewCalculator.java
@@ -19,10 +19,10 @@ public class NewCalculator {
         return this.lastResult;
     }
 
-    public double Divide(double dividend, double divisor){
-        this.lastResult = dividend % divisor;
+    public double Divide(double dividend, double divisor){ 
+        this.lastResult = dividend / divisor;
         return this.lastResult;
-    }
+        }
 
     public double Multiply(double x, double y){
         this.lastResult = x * y;
